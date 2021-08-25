8d67fe94ce7583a197140464ec74ceaada09cd5e
Guillermo Velasco <alfadoble10@gmail.com>
2021-08-24 21:37:07 -0300

    2.7 - Agregado Listado, Condicion para evitar duplicados

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+
+  const setNewNameTel=(e)=>{
+    setNewName(e.target.value)
+  }
+
+  const addPerson=(e)=>{
+    e.preventDefault()
+    if(persons.find(element=> element.name===newName)){
+      alert(`${newName} is already added to phonebook`)
+      setNewName('')
+      return
+    }
+
+    const newPerson={
+      name:newName
+    }
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  } //2.6 paso 1
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={setNewNameTel} />
+          debug:{newName}
+        </div>
+        <div>
+          <button type="submit" >add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {
+          persons.map((reg) => 
+            <li key={reg.name}>{reg.name}</li>          
+          )
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
