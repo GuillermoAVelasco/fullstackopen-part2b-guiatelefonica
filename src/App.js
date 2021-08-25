611584d2d85f41cc6cb877f8f745dbd231749048
Guillermo Velasco <alfadoble10@gmail.com>
2021-08-24 22:11:54 -0300

    2.8 Incorporacion campo numero de telefono, y campo de busqueda

--- src/App.js
+++ src/App.js
@@ -2,37 +2,54 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number:'040-1234567'}
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filterName, setFilterName ] = useState('')
 
   const setNewNameTel=(e)=>{
     setNewName(e.target.value)
   }
 
+  const setNewNumberTel=(e)=>{
+    setNewNumber(e.target.value)
+  }
+
+  const setFindFilterName=(e)=>{
+    setFilterName(e.target.value)
+  }
+
   const addPerson=(e)=>{
     e.preventDefault()
-    if(persons.find(element=> element.name===newName)){
+    if(persons.find(element=> element.name.trim().toUpperCase()===newName.trim().toUpperCase())){
       alert(`${newName} is already added to phonebook`)
       setNewName('')
+      setNewNumber('')
       return
     }
 
     const newPerson={
-      name:newName
+      name:newName,
+      number:newNumber
     }
     setPersons(persons.concat(newPerson))
     setNewName('')
+    setNewNumber('')
   } //2.6 paso 1
 
+  const personsFind = filterName===''
+  ? persons
+  : persons.filter(person => person.name.toUpperCase().indexOf(filterName.toUpperCase()) !== -1)
+console.log(personsFind)
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter show with: <input value={filterName} onChange={setFindFilterName} /></div>
+      <h2>Add a New</h2>
       <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={setNewNameTel} />
-          debug:{newName}
-        </div>
+        <div>name: <input value={newName} onChange={setNewNameTel} /></div>
+        <div>number: <input value={newNumber} onChange={setNewNumberTel} /></div>
         <div>
           <button type="submit" >add</button>
         </div>
@@ -40,8 +57,8 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {
-          persons.map((reg) => 
-            <li key={reg.name}>{reg.name}</li>          
+          personsFind.map((reg) => 
+            <li key={reg.name}>{reg.name} {reg.number}</li>          
           )
         }
       </ul>
